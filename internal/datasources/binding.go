e98725306b6c4a03ac37128c736b32734c2b17e3
Alberto Donato <alberto@stacklet.io>
2025-05-14 13:01:40 +0200

    chore: rework binding resource and data source [ENG-5065]
    [ENG-5065](https://stacklet.atlassian.net/browse/ENG-5065)
    [ENG-5073](https://stacklet.atlassian.net/browse/ENG-5073)
    
    ### what
    
    rework code for binding resource and data source
    
    ### why
    
    bring it in line with other resources
    
    ### testing
    
    unit tests and testing in sandbox
    
    ### docs
    
    updated here

--- internal/datasources/binding.go
+++ internal/datasources/binding.go
@@ -3,12 +3,16 @@ package datasources
 import (
 	"context"
 	"fmt"
-	"math/big"
 
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hasura/go-graphql-client"
+
+	"github.com/stacklet/terraform-provider-stacklet/internal/api"
+	"github.com/stacklet/terraform-provider-stacklet/internal/helpers"
+	"github.com/stacklet/terraform-provider-stacklet/internal/models"
+	tftypes "github.com/stacklet/terraform-provider-stacklet/internal/types"
 )
 
 var (
@@ -20,23 +24,7 @@ func NewBindingDataSource() datasource.DataSource {
 }
 
 type bindingDataSource struct {
-	client *graphql.Client
-}
-
-type bindingDataSourceModel struct {
-	ID                   types.String `tfsdk:"id"`
-	UUID                 types.String `tfsdk:"uuid"`
-	Name                 types.String `tfsdk:"name"`
-	Description          types.String `tfsdk:"description"`
-	AutoDeploy           types.Bool   `tfsdk:"auto_deploy"`
-	System               types.Bool   `tfsdk:"system"`
-	Schedule             types.String `tfsdk:"schedule"`
-	Variables            types.String `tfsdk:"variables"`
-	LastDeployed         types.String `tfsdk:"last_deployed"`
-	AccountGroupUUID     types.String `tfsdk:"account_group_uuid"`
-	PolicyCollectionUUID types.String `tfsdk:"policy_collection_uuid"`
-	PercentageDeployed   types.Number `tfsdk:"percentage_deployed"`
-	IsStale              types.Bool   `tfsdk:"is_stale"`
+	api *api.API
 }
 
 func (d *bindingDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
@@ -67,20 +55,12 @@ func (d *bindingDataSource) Schema(_ context.Context, _ datasource.SchemaRequest
 				Description: "Whether the binding automatically deploys when the policy collection changes.",
 				Computed:    true,
 			},
-			"system": schema.BoolAttribute{
-				Description: "Whether this is a system binding (not user editable).",
-				Computed:    true,
-			},
 			"schedule": schema.StringAttribute{
 				Description: "The schedule for the binding (e.g., 'rate(1 hour)', 'rate(2 hours)', or cron expression).",
 				Computed:    true,
 			},
 			"variables": schema.StringAttribute{
 				Description: "JSON-encoded dictionary of values used for policy templating.",
-				Computed:    true,
-			},
-			"last_deployed": schema.StringAttribute{
-				Description: "The timestamp of the last deployment.",
 				Computed:    true,
 			},
 			"account_group_uuid": schema.StringAttribute{
@@ -91,12 +71,8 @@ func (d *bindingDataSource) Schema(_ context.Context, _ datasource.SchemaRequest
 				Description: "The UUID of the policy collection this binding applies.",
 				Computed:    true,
 			},
-			"percentage_deployed": schema.NumberAttribute{
-				Description: "The percentage of accounts where the binding is deployed (0-100).",
-				Computed:    true,
-			},
-			"is_stale": schema.BoolAttribute{
-				Description: "Whether the binding has pending changes that need to be deployed.",
+			"system": schema.BoolAttribute{
+				Description: "Whether this is a system binding.",
 				Computed:    true,
 			},
 		},
@@ -117,76 +93,37 @@ func (d *bindingDataSource) Configure(_ context.Context, req datasource.Configur
 		return
 	}
 
-	d.client = client
+	d.api = api.New(client)
 }
 
 func (d *bindingDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	var data bindingDataSourceModel
+	var data models.BindingDataSource
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// GraphQL query
-	var query struct {
-		Binding struct {
-			ID           string
-			UUID         string
-			Name         string
-			Description  string
-			AutoDeploy   bool
-			System       bool
-			Schedule     string
-			Variables    string
-			LastDeployed string
-			AccountGroup struct {
-				UUID string
-			}
-			PolicyCollection struct {
-				UUID string
-			}
-			PercentageDeployed float64
-			IsStale            bool
-		} `graphql:"binding(uuid: $uuid, name: $name)"`
-	}
-
-	variables := map[string]any{
-		"uuid": (*string)(nil),
-		"name": (*string)(nil),
-	}
-
-	if !data.UUID.IsNull() {
-		variables["uuid"] = graphql.String(data.UUID.ValueString())
-	}
-
-	if !data.Name.IsNull() {
-		variables["name"] = graphql.String(data.Name.ValueString())
-	}
-
-	err := d.client.Query(ctx, &query, variables)
+	binding, err := d.api.Binding.Read(ctx, data.UUID.ValueString(), data.Name.ValueString())
 	if err != nil {
-		resp.Diagnostics.AddError("Client Error", fmt.Sprintf("Unable to read binding, got error: %s", err))
+		helpers.AddDiagError(&resp.Diagnostics, err)
 		return
 	}
 
-	if query.Binding.UUID == "" {
-		resp.Diagnostics.AddError("Not Found", "No binding found with the specified UUID or name")
+	data.ID = types.StringValue(binding.ID)
+	data.UUID = types.StringValue(binding.UUID)
+	data.Name = types.StringValue(binding.Name)
+	data.Description = tftypes.NullableString(binding.Description)
+	data.AutoDeploy = types.BoolValue(binding.AutoDeploy)
+	data.Schedule = tftypes.NullableString(binding.Schedule)
+	data.AccountGroupUUID = types.StringValue(binding.AccountGroup.UUID)
+	data.PolicyCollectionUUID = types.StringValue(binding.PolicyCollection.UUID)
+	data.System = types.BoolValue(binding.System)
+	variablesString, err := tftypes.JSONString(binding.ExecutionConfig.Variables)
+	if err != nil {
+		resp.Diagnostics.AddError("Invalid content for variables", err.Error())
 		return
 	}
-
-	data.ID = types.StringValue(query.Binding.ID)
-	data.UUID = types.StringValue(query.Binding.UUID)
-	data.Name = types.StringValue(query.Binding.Name)
-	data.Description = types.StringValue(query.Binding.Description)
-	data.AutoDeploy = types.BoolValue(query.Binding.AutoDeploy)
-	data.System = types.BoolValue(query.Binding.System)
-	data.Schedule = types.StringValue(query.Binding.Schedule)
-	data.Variables = types.StringValue(query.Binding.Variables)
-	data.LastDeployed = types.StringValue(query.Binding.LastDeployed)
-	data.AccountGroupUUID = types.StringValue(query.Binding.AccountGroup.UUID)
-	data.PolicyCollectionUUID = types.StringValue(query.Binding.PolicyCollection.UUID)
-	data.PercentageDeployed = types.NumberValue(big.NewFloat(query.Binding.PercentageDeployed))
-	data.IsStale = types.BoolValue(query.Binding.IsStale)
+	data.Variables = variablesString
 
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
---
+++ internal/models/binding.go
@@ -0,0 +1,31 @@
+package models
+
+import (
+	"github.com/hashicorp/terraform-plugin-framework/types"
+)
+
+type BindingResource struct {
+	ID                   types.String `tfsdk:"id"`
+	UUID                 types.String `tfsdk:"uuid"`
+	Name                 types.String `tfsdk:"name"`
+	Description          types.String `tfsdk:"description"`
+	AutoDeploy           types.Bool   `tfsdk:"auto_deploy"`
+	Schedule             types.String `tfsdk:"schedule"`
+	Variables            types.String `tfsdk:"variables"`
+	AccountGroupUUID     types.String `tfsdk:"account_group_uuid"`
+	PolicyCollectionUUID types.String `tfsdk:"policy_collection_uuid"`
+	System               types.Bool   `tfsdk:"system"`
+}
+
+type BindingDataSource struct {
+	ID                   types.String `tfsdk:"id"`
+	UUID                 types.String `tfsdk:"uuid"`
+	Name                 types.String `tfsdk:"name"`
+	Description          types.String `tfsdk:"description"`
+	AutoDeploy           types.Bool   `tfsdk:"auto_deploy"`
+	Schedule             types.String `tfsdk:"schedule"`
+	Variables            types.String `tfsdk:"variables"`
+	AccountGroupUUID     types.String `tfsdk:"account_group_uuid"`
+	PolicyCollectionUUID types.String `tfsdk:"policy_collection_uuid"`
+	System               types.Bool   `tfsdk:"system"`
+}
