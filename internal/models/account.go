36671ff5eda078fc201216d4201c78dbb3f7d54a
Alberto Donato <albertodonato@users.noreply.github.com>
2025-05-27 13:30:00 +0200

    feat: handle account security context as a WO secret [ENG-5163] (#47)
    [ENG-5163](https://stacklet.atlassian.net/browse/ENG-5163)
    
    ### what
    
    - replace the plain `security_context` attribute in the account resource with a
    pair of `_wo` and `_wo_version` to handle it as a secret.
    - keep the `security_context` attribute as a computed one in both resource and
    data source
    - use JSONString to normalize variables string
    
    ### why
    
    avoid storing the security context secret input in the state
    
    ### testing
    
    local testing in sandbox
    
    ### docs
    
    updated here

--- internal/models/account.go
+++ internal/models/account.go
@@ -6,6 +6,22 @@ import (
 
 // AccountResource is the model for account resources.
 type AccountResource struct {
+	ID                       types.String `tfsdk:"id"`
+	Key                      types.String `tfsdk:"key"`
+	Name                     types.String `tfsdk:"name"`
+	ShortName                types.String `tfsdk:"short_name"`
+	Description              types.String `tfsdk:"description"`
+	CloudProvider            types.String `tfsdk:"cloud_provider"`
+	Path                     types.String `tfsdk:"path"`
+	Email                    types.String `tfsdk:"email"`
+	SecurityContext          types.String `tfsdk:"security_context"`
+	SecurityContextWO        types.String `tfsdk:"security_context_wo"`
+	SecurityContextWOVersion types.String `tfsdk:"security_context_wo_version"`
+	Variables                types.String `tfsdk:"variables"`
+}
+
+// AcountDataSourcemodel is the model for account data sources.
+type AccountDataSource struct {
 	ID              types.String `tfsdk:"id"`
 	Key             types.String `tfsdk:"key"`
 	Name            types.String `tfsdk:"name"`
@@ -17,6 +33,3 @@ type AccountResource struct {
 	SecurityContext types.String `tfsdk:"security_context"`
 	Variables       types.String `tfsdk:"variables"`
 }
-
-// AcountDataSourcemodel is the model for account data sources.
-type AccountDataSource AccountResource
